d447808a5f8cf80100c42db708eaa87d955b8078
Damisha74 <130962043+Damisha74@users.noreply.github.com>
2025-01-16 17:06:37 +0530

    Update Resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -15,7 +15,7 @@ const Resume = () => {
     >
       <FadeIn>
         <div className="flex justify-center items-center text-center">
-          <Title des="Resume" />
+          <Title des="Qualifications" />
         </div>
         <div>
           <ul className="w-full grid grid-cols-1 md:grid-cols-2">
