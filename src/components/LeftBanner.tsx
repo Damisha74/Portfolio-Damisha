18272009cd13250d3666348729101e49660544ac
Damisha74 <130962043+Damisha74@users.noreply.github.com>
2025-01-16 17:28:24 +0530

    update LeftBanner.tsx

--- src/components/LeftBanner.tsx
+++ src/components/LeftBanner.tsx
@@ -1,31 +1,31 @@
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { FaGithub, FaMedium, FaLinkedinIn, FaReact, FaNodeJs } from "react-icons/fa";
-import {  SiFigma,  } from "react-icons/si";
+import { SiFigma } from "react-icons/si";
 import { DiMongodb } from "react-icons/di";
-
 import { FadeIn } from "./FadeIn";
 
 const LeftBanner = () => {
   const [text] = useTypewriter({
-    words: ["IT Undergraduate.", "Full Stack Developer."],
+    words: ["an IT Undergraduate.", "a Full Stack Developer."],
     loop: true,
     typeSpeed: 20,
     deleteSpeed: 10,
     delaySpeed: 2000,
   });
+
   return (
     <FadeIn className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
-        <h4 className=" text-lg font-normal">WELCOME</h4>
+        <h4 className="text-lg font-normal">WELCOME</h4>
         <h1 className="text-5xl font-bold text-white">
           Hi, I'm <span className="text-designColor capitalize">Damisha Wickramaarachchi</span>
         </h1>
         <h2 className="text-3xl font-bold text-white">
-          a <span>{text}</span>
+          {text}
           <Cursor cursorStyle="|" cursorColor="#6a329f" />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wider">
-          I am a third year IT undergraduate at the University of Moratuwa. I am
+          I am a third-year IT undergraduate at the University of Moratuwa. I am
           passionate about Full Stack Development.
         </p>
       </div>
